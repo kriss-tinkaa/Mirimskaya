aad945d81d5a71144a914e05806ae4d1a026ecdf
Kristina <kriss.tinka03@gmail.com>
2021-08-20 07:35:52 +0300

    desktop part

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// initiative items
+function toggleItem(elem) {
+    for (var i = 0; i < elem.length; i++) {
+      elem[i].addEventListener("click", function(e) {
+        var current = this;
+        for (var i = 0; i < elem.length; i++) {
+          if (current != elem[i]) {
+            elem[i].classList.remove('active');
+          } else if (current.classList.contains('active') === true) {
+            current.classList.remove('active');
+          } else {
+            current.classList.add('active')
+          }
+        }
+        e.preventDefault();
+      });
+    };
+  }
+  toggleItem(document.querySelectorAll('.initiative__item'));
+
+// smoothScroll
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
